a735c22c64a57260ac3a2e96341926192b12e00e
Stephen Fegan <sfegan@gmail.com>
2018-02-19 14:36:52 +0100

    Add copyright notice and GPL license

--- dft_simd.cpp
+++ dft_simd.cpp
@@ -1,3 +1,25 @@
+/*
+
+   fftw_simd/fftw_simd.cpp -- Stephen Fegan -- 2018-02-19
+
+   Test drive for FFTW speed tests and for SIMD genfft codelets
+
+   Copyright 2018, Stephen Fegan <sfegan@llr.in2p3.fr>
+   LLR, Ecole Polytechnique, CNRS/IN2P3
+
+   This file is part of "fftw_simd"
+
+   "fftw_simd" is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License version 2 or
+   later, as published by the Free Software Foundation.
+
+   "fftw_simd" is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+*/
+
 #include <immintrin.h>
 #include <fftw3.h>
 #include <random>
