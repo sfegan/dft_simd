2cda960a59fde1f13aa534cbfadeab8399316bac
Stephen Fegan <sfegan@gmail.com>
2018-03-06 10:57:28 +0100

    Allow compilation on non FMA systems (I'm looking at you Travis)

--- dft_simd.cpp
+++ dft_simd.cpp
@@ -26,8 +26,6 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
-#if defined(__AVX__) and defined(__FMA__)
-
 constexpr int nvec = 8;
 constexpr int nsamp = 60;
 //constexpr int nsamp_align = (nsamp+nvec-1)/nvec * nvec;
@@ -181,38 +179,84 @@ TEST(TestDFT, FFTW_SuperAligned_Eight)
 }
 #endif
 
+#if defined(__AVX__)
+
+using INT = int;
+using stride = int;
+
+inline __m256 ADD(const __m256 a, const __m256 b) { return _mm256_add_ps(a,b); }
+inline __m256 SUB(const __m256 a, const __m256 b) { return _mm256_sub_ps(a,b); }
+inline __m256 MUL(const __m256 a, const __m256 b) { return _mm256_mul_ps(a,b); }
+
+inline __m256 NEG(const __m256 a) { return _mm256_xor_ps(a, _mm256_set1_ps(-0.0)); }
+
+#if defined(__FMA__)
+inline __m256 FMA(const __m256 a, const __m256 b, const __m256 c) {
+  return _mm256_fmadd_ps(a,b,c); }
+inline __m256 FMS(const __m256 a, const __m256 b, const __m256 c) {
+  return _mm256_fmsub_ps(a,b,c); }
+// Note: inconsistency between FFTW and Intel intrinsics definitions of FNMA/S
+inline __m256 FNMA(const __m256 a, const __m256 b, const __m256 c) {
+  return _mm256_fnmsub_ps(a,b,c); }
+inline __m256 FNMS(const __m256 a, const __m256 b, const __m256 c) {
+  return _mm256_fnmadd_ps(a,b,c); }
+#else
+inline __m256 FMA(const __m256 a, const __m256 b, const __m256 c) {
+  return _mm256_add_ps(_mm256_mul_ps(a,b),c); }
+inline __m256 FMS(const __m256 a, const __m256 b, const __m256 c) {
+  return _mm256_sub_ps(_mm256_mul_ps(a,b),c); }
+inline __m256 FNMA(const __m256 a, const __m256 b, const __m256 c) {
+  return _mm256_sub_ps(NEG(_mm256_mul_ps(a,b)),c); }
+inline __m256 FNMS(const __m256 a, const __m256 b, const __m256 c) {
+  return _mm256_add_ps(NEG(_mm256_mul_ps(a,b)),c); }
+#endif
+
+inline std::pair<__m256,__m256> ADD(const std::pair<__m256,__m256> a, const std::pair<__m256,__m256> b) {
+  return { ADD(a.first,b.first), ADD(a.second,b.second) }; }
+inline std::pair<__m256,__m256> SUB(const std::pair<__m256,__m256> a, const std::pair<__m256,__m256> b) {
+  return { SUB(a.first,b.first), SUB(a.second,b.second) }; }
+inline std::pair<__m256,__m256> MUL(const std::pair<__m256,__m256> a, const std::pair<__m256,__m256> b) {
+  return { MUL(a.first,b.first), MUL(a.second,b.second) }; }
+
+//inline E NEG(const E a) { return _mm256_sub_ps(_mm256_setzero_ps(),a); }
+inline std::pair<__m256,__m256> NEG(const std::pair<__m256,__m256> a) {
+  return { NEG(a.first), NEG(a.second) }; }
+
+inline std::pair<__m256,__m256> FMA(const std::pair<__m256,__m256> a, const std::pair<__m256,__m256> b, const std::pair<__m256,__m256> c) {
+  return { FMA(a.first,b.first,c.first), FMA(a.second,b.second,c.second) }; }
+inline std::pair<__m256,__m256> FMS(const std::pair<__m256,__m256> a, const std::pair<__m256,__m256> b, const std::pair<__m256,__m256> c) {
+  return { FMS(a.first,b.first,c.first), FMS(a.second,b.second,c.second) }; }
+// Note: inconsistency between FFTW and Intel intrinsics definitions of FNMA/S
+inline std::pair<__m256,__m256> FNMA(const std::pair<__m256,__m256> a, const std::pair<__m256,__m256> b, const std::pair<__m256,__m256> c) {
+  return { FNMA(a.first,b.first,c.first), FNMA(a.second,b.second,c.second) }; }
+inline std::pair<__m256,__m256> FNMS(const std::pair<__m256,__m256> a, const std::pair<__m256,__m256> b, const std::pair<__m256,__m256> c) {
+  return { FNMS(a.first,b.first,c.first), FNMS(a.second,b.second,c.second) }; }
+
+inline std::pair<__m256,__m256> MUL(const __m256 a, const std::pair<__m256,__m256> b) {
+  return { MUL(a,b.first), MUL(a,b.second) }; }
+inline std::pair<__m256,__m256> FMA(const __m256 a, const std::pair<__m256,__m256> b, const std::pair<__m256,__m256> c) {
+  return { FMA(a,b.first,c.first), FMA(a,b.second,c.second) }; }
+inline std::pair<__m256,__m256> FMS(const __m256 a, const std::pair<__m256,__m256> b, const std::pair<__m256,__m256> c) {
+  return { FMS(a,b.first,c.first), FMS(a,b.second,c.second) }; }
+inline std::pair<__m256,__m256> FNMA(const __m256 a, const std::pair<__m256,__m256> b, const std::pair<__m256,__m256> c) {
+  return { FNMA(a,b.first,c.first), FNMA(a,b.second,c.second) }; }
+inline std::pair<__m256,__m256> FNMS(const __m256 a, const std::pair<__m256,__m256> b, const std::pair<__m256,__m256> c) {
+  return { FNMS(a,b.first,c.first), FNMS(a,b.second,c.second) }; }
+
+#define DK(name, val) \
+  static const __m256 name = { (val),(val),(val),(val),(val),(val),(val),(val) }
+
+inline void MAKE_VOLATILE_STRIDE(int a, int b) { }
+
 namespace m256 {
 
 using E = __m256;
 using R = __m256;
-using INT = int;
-using stride = int;
 
 inline int WS(const stride s, const stride i) { return s*i; }
 
-inline E ADD(const E a, const E b) { return _mm256_add_ps(a,b); }
-inline E SUB(const E a, const E b) { return _mm256_sub_ps(a,b); }
-inline E MUL(const E a, const E b) { return _mm256_mul_ps(a,b); }
-
-//inline E NEG(const E a) { return _mm256_sub_ps(_mm256_setzero_ps(),a); }
-inline E NEG(const E a) { return _mm256_xor_ps(a, _mm256_set1_ps(-0.0)); }
-
-inline E FMA(const E a, const E b, const E c) { return _mm256_fmadd_ps(a,b,c); }
-inline E FMS(const E a, const E b, const E c) { return _mm256_fmsub_ps(a,b,c); }
-// Note: inconsistency between FFTW and Intel intrinsics definitions of FNMA/S
-inline E FNMA(const E a, const E b, const E c) { return _mm256_fnmsub_ps(a,b,c); }
-inline E FNMS(const E a, const E b, const E c) { return _mm256_fnmadd_ps(a,b,c); }
-
-#define DK(name, val) \
-  static const E name = { (val),(val),(val),(val),(val),(val),(val),(val) }
-
-inline void MAKE_VOLATILE_STRIDE(int a, int b) { }
-
-//#include "dft_c2c_60.c"
 #include "dft_r2cf_60.c"
 #include "dft_r2cb_60.c"
-
-#undef DK
 
 } // namespace m256
 
@@ -247,34 +291,11 @@ namespace m256_FixedStride {
 
 using E = __m256;
 using R = __m256;
-using INT = int;
-using stride = int;
 
 inline int WS(const stride s, const stride i) { return 2*i; }
 
-inline E ADD(const E a, const E b) { return _mm256_add_ps(a,b); }
-inline E SUB(const E a, const E b) { return _mm256_sub_ps(a,b); }
-inline E MUL(const E a, const E b) { return _mm256_mul_ps(a,b); }
-
-//inline E NEG(const E a) { return _mm256_sub_ps(_mm256_setzero_ps(),a); }
-inline E NEG(const E a) { return _mm256_xor_ps(a, _mm256_set1_ps(-0.0)); }
-
-inline E FMA(const E a, const E b, const E c) { return _mm256_fmadd_ps(a,b,c); }
-inline E FMS(const E a, const E b, const E c) { return _mm256_fmsub_ps(a,b,c); }
-// Note: inconsistency between FFTW and Intel intrinsics definitions of FNMA/S
-inline E FNMA(const E a, const E b, const E c) { return _mm256_fnmsub_ps(a,b,c); }
-inline E FNMS(const E a, const E b, const E c) { return _mm256_fnmadd_ps(a,b,c); }
-
-#define DK(name, val) \
-  static const E name = { (val),(val),(val),(val),(val),(val),(val),(val) }
-
-inline void MAKE_VOLATILE_STRIDE(int a, int b) { }
-
-//#include "dft_c2c_60.c"
 #include "dft_r2cf_60.c"
 #include "dft_r2cb_60.c"
-
-#undef DK
 
 } // namespace m256
 
@@ -314,49 +335,8 @@ using stride = int;
 
 inline int WS(const stride s, const stride i) { return s*i; }
 
-inline E ADD(const E a, const E b) {
-  return { _mm256_add_ps(a.first,b.first), _mm256_add_ps(a.second,b.second) }; }
-inline E SUB(const E a, const E b) {
-  return { _mm256_sub_ps(a.first,b.first), _mm256_sub_ps(a.second,b.second) }; }
-inline E MUL(const E a, const E b) {
-  return { _mm256_mul_ps(a.first,b.first), _mm256_mul_ps(a.second,b.second) }; }
-
-//inline E NEG(const E a) { return _mm256_sub_ps(_mm256_setzero_ps(),a); }
-inline E NEG(const E a) {
-  return { _mm256_xor_ps(a.first, _mm256_set1_ps(-0.0)), _mm256_xor_ps(a.second, _mm256_set1_ps(-0.0)) }; }
-
-inline E FMA(const E a, const E b, const E c) {
-  return { _mm256_fmadd_ps(a.first,b.first,c.first), _mm256_fmadd_ps(a.second,b.second,c.second) }; }
-inline E FMS(const E a, const E b, const E c) {
-  return { _mm256_fmsub_ps(a.first,b.first,c.first), _mm256_fmsub_ps(a.second,b.second,c.second) }; }
-// Note: inconsistency between FFTW and Intel intrinsics definitions of FNMA/S
-inline E FNMA(const E a, const E b, const E c) {
-  return { _mm256_fnmsub_ps(a.first,b.first,c.first),_mm256_fnmsub_ps(a.second,b.second,c.second) }; }
-inline E FNMS(const E a, const E b, const E c) {
-  return { _mm256_fnmadd_ps(a.first,b.first,c.first),_mm256_fnmadd_ps(a.second,b.second,c.second) }; }
-
-inline E MUL(const __m256 a, const E b) {
-  return { _mm256_mul_ps(a,b.first), _mm256_mul_ps(a,b.second) }; }
-inline E FMA(const __m256 a, const E b, const E c) {
-  return { _mm256_fmadd_ps(a,b.first,c.first), _mm256_fmadd_ps(a,b.second,c.second) }; }
-inline E FMS(const __m256 a, const E b, const E c) {
-  return { _mm256_fmsub_ps(a,b.first,c.first), _mm256_fmsub_ps(a,b.second,c.second) }; }
-// Note: inconsistency between FFTW and Intel intrinsics definitions of FNMA/S
-inline E FNMA(const __m256 a, const E b, const E c) {
-  return { _mm256_fnmsub_ps(a,b.first,c.first),_mm256_fnmsub_ps(a,b.second,c.second) }; }
-inline E FNMS(const __m256 a, const E b, const E c) {
-  return { _mm256_fnmadd_ps(a,b.first,c.first),_mm256_fnmadd_ps(a,b.second,c.second) }; }
-
-#define DK(name, val) \
-  static const __m256 name = { (val),(val),(val),(val),(val),(val),(val),(val) }
-
-inline void MAKE_VOLATILE_STRIDE(int a, int b) { }
-
-//#include "dft_c2c_60.c"
 #include "dft_r2cf_60.c"
 #include "dft_r2cb_60.c"
-
-#undef DK
 
 } // namespace m256
 
@@ -395,54 +375,11 @@ namespace m256_Unroll2_FixedStride {
 
 using E = std::pair<__m256,__m256>;
 using R = std::pair<__m256,__m256>;
-using INT = int;
-using stride = int;
 
 inline int WS(const stride s, const stride i) { return 2*i; }
 
-inline E ADD(const E a, const E b) {
-  return { _mm256_add_ps(a.first,b.first), _mm256_add_ps(a.second,b.second) }; }
-inline E SUB(const E a, const E b) {
-  return { _mm256_sub_ps(a.first,b.first), _mm256_sub_ps(a.second,b.second) }; }
-inline E MUL(const E a, const E b) {
-  return { _mm256_mul_ps(a.first,b.first), _mm256_mul_ps(a.second,b.second) }; }
-
-//inline E NEG(const E a) { return _mm256_sub_ps(_mm256_setzero_ps(),a); }
-inline E NEG(const E a) {
-  return { _mm256_xor_ps(a.first, _mm256_set1_ps(-0.0)), _mm256_xor_ps(a.second, _mm256_set1_ps(-0.0)) }; }
-
-inline E FMA(const E a, const E b, const E c) {
-  return { _mm256_fmadd_ps(a.first,b.first,c.first), _mm256_fmadd_ps(a.second,b.second,c.second) }; }
-inline E FMS(const E a, const E b, const E c) {
-  return { _mm256_fmsub_ps(a.first,b.first,c.first), _mm256_fmsub_ps(a.second,b.second,c.second) }; }
-// Note: inconsistency between FFTW and Intel intrinsics definitions of FNMA/S
-inline E FNMA(const E a, const E b, const E c) {
-  return { _mm256_fnmsub_ps(a.first,b.first,c.first),_mm256_fnmsub_ps(a.second,b.second,c.second) }; }
-inline E FNMS(const E a, const E b, const E c) {
-  return { _mm256_fnmadd_ps(a.first,b.first,c.first),_mm256_fnmadd_ps(a.second,b.second,c.second) }; }
-
-inline E MUL(const __m256 a, const E b) {
-  return { _mm256_mul_ps(a,b.first), _mm256_mul_ps(a,b.second) }; }
-inline E FMA(const __m256 a, const E b, const E c) {
-  return { _mm256_fmadd_ps(a,b.first,c.first), _mm256_fmadd_ps(a,b.second,c.second) }; }
-inline E FMS(const __m256 a, const E b, const E c) {
-  return { _mm256_fmsub_ps(a,b.first,c.first), _mm256_fmsub_ps(a,b.second,c.second) }; }
-// Note: inconsistency between FFTW and Intel intrinsics definitions of FNMA/S
-inline E FNMA(const __m256 a, const E b, const E c) {
-  return { _mm256_fnmsub_ps(a,b.first,c.first),_mm256_fnmsub_ps(a,b.second,c.second) }; }
-inline E FNMS(const __m256 a, const E b, const E c) {
-  return { _mm256_fnmadd_ps(a,b.first,c.first),_mm256_fnmadd_ps(a,b.second,c.second) }; }
-
-#define DK(name, val) \
-  static const __m256 name = { (val),(val),(val),(val),(val),(val),(val),(val) }
-
-inline void MAKE_VOLATILE_STRIDE(int a, int b) { }
-
-//#include "dft_c2c_60.c"
 #include "dft_r2cf_60.c"
 #include "dft_r2cb_60.c"
-
-#undef DK
 
 } // namespace m256
 
